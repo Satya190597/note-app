57f793b79a3345a7c76732770e6e97cd1bef27ca
satyan <satyan@mindfiresolutions.com>
2022-07-31 02:32:20 +0530

    Save Data To Local Storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
@@ -13,6 +13,16 @@ const App = () => {
   const [searchText, setSearchText] = useState("");
 
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));        
+    setNotes(savedNotes ? savedNotes : []);
+    
+  }, []);
+
+  useEffect(() => {    
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+  }, [notes]);
 
   const addNote = (text) => {
     const date = new Date();
@@ -31,7 +41,7 @@ const App = () => {
   };
 
   return (
-    <div className={`${darkMode && 'dark-mode'}`}>
+    <div className={`${darkMode && "dark-mode"}`}>
       <div className="container">
         <Header handleToggleDarkMode={setDarkMode} />
         <Search handleSearchText={setSearchText} text={searchText} />
