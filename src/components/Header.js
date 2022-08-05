4e7f7b051336a8792cb2d1ef1679e236e40fb88b
satyan <satyan@mindfiresolutions.com>
2022-08-05 19:03:27 +0530

    Made UI Changes For Header And Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
-const Header = ({ handleToggleDarkMode,mode }) => {
+const Header = ({ handleToggleDarkMode, mode }) => {
   return (
     <>
       <div className="header">
         <h1>Notes</h1>
-        <button
-          className={`${mode && "save-dark"} ${!mode && "save"}`}
-          onClick={() =>
-            handleToggleDarkMode((previousState) => !previousState)
-          }
-        >
-          Toggle Mode
-        </button>
+        {mode && (
+          <div onClick={() => handleToggleDarkMode((value) => !value)}>
+            <MdLightMode
+              size="1.3em"
+              style={{ color: "white", cursor: "pointer" }}
+            />
+          </div>
+        )}
+        {!mode && (
+          <div onClick={() => handleToggleDarkMode((value) => !value)}>
+            <MdDarkMode
+              size="1.3em"
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        )}
       </div>
     </>
   );
